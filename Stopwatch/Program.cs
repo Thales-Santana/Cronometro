1ad31aca2ea26fd4f31a46cf0fe0c3f428c69a9c
Thales-Santana <58236013+Thales-Santana@users.noreply.github.com>
2024-01-31 17:30:22 -0300

    Stopwatch project creation

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+
+namespace Stopwatch
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Menu();
+    }
+
+    static void Menu()
+    {
+      Console.Clear();
+      Console.WriteLine("digite S para segundos ou M para minutos");
+      string userType = Console.ReadLine().ToLower();
+
+      Console.WriteLine("Quanto tempo deseja contar?");
+      int userTime = int.Parse(Console.ReadLine());
+
+      Start(userTime, userType);
+    }
+
+    static void PreStart()
+    {
+      Console.Clear();
+      Console.WriteLine("Ready?");
+      Thread.Sleep(1000);
+      Console.WriteLine("Set...");
+      Thread.Sleep(1000);
+      Console.WriteLine("GO!");
+      Thread.Sleep(1500);
+    }
+    static void Start(int time, string type)
+    {
+      PreStart();
+      int currentTime = 0;
+
+      if(type == "m"){
+        time *= 60;
+      }
+
+      while (currentTime != time)
+      {
+        Console.Clear();
+        currentTime++;
+        Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+      }
+      Console.Clear();
+      Console.WriteLine("Finished");
+    }
+  }
+}
